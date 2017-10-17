8b652f0f69616bd932aca9528eae0746b9fcf47e
DESKTOP-1IGS2AN\Niall <niall@food-surveys.com>
2017-10-17 14:42:04 +0100

    corrected include location again, and a new php array file

---
+++ htdocs/messaround/test.php
@@ -0,0 +1,19 @@
+<?php
+include('C:\xampp\connection\connect.php');
+
+$newactive  = array('eating' => array('people' => 'john', 'jill',
+									  'number' => 	1, 2),
+
+					'sleeping' => array('people' => 'john', 'jill',
+									  'number' => 	1, 2),
+
+					'walking' => array('people' => 'john', 'jill',
+									  'number' => 	1, 2),
+
+					'sitting' => array('people' => 'john', 'jill',
+									  'number' => 	1, 2));
+
+
+print_r($newactive);
+
+?>
