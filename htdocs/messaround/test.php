f79bf1a2c913e6cb4a7fd1b9dc7a42639d059f4e
DESKTOP-1IGS2AN\Niall <niall@food-surveys.com>
2017-10-17 16:43:18 +0100

    updated

--- htdocs/messaround/test.php
+++ htdocs/messaround/test.php
@@ -1,19 +1,40 @@
 <?php
 include('C:\xampp\connection\connect.php');
 
-$newactive  = array('eating' => array('people' => 'john', 'jill',
-									  'number' => 	1, 2),
+$newquery = mysqli_query($conn, "SELECT * FROM `personactivities` LEFT JOIN `personname` ON `personactivities`.`personid` = `personname`.`personid` ORDER BY `activity` ASC ");
 
-					'sleeping' => array('people' => 'john', 'jill',
-									  'number' => 	1, 2),
-
-					'walking' => array('people' => 'john', 'jill',
-									  'number' => 	1, 2),
-
-					'sitting' => array('people' => 'john', 'jill',
-									  'number' => 	1, 2));
+$qresult = array();
 
 
-print_r($newactive);
 
+while ($row = mysqli_fetch_assoc($newquery)) {
+    $array[] = $row;
+
+$array[] = array('personname'=>$row['personname'], 'hours'=>$row['hours']);
+
+
+$newactive  = array('eating' => array('people' => $array[0]['personname'], $array[1]['personname'],
+									  'number' => 	$array[0]['hours'], $array[1]['hours']),
+
+
+					'sleeping' => array('people' => $array[1]['personname'], $array[0]['personname'],
+									  'number' => 	$array[13]['hours'], $array[2]['hours']),
+
+
+					'walking' => array('people' => '', '',
+									  'number' => 	5, 5),
+
+
+					'sitting' => array('people' => '', '',
+									  'number' => 	5, 5));
+
+}
+
+
+
+
+
+
+var_dump($newactive);
 ?>
+
